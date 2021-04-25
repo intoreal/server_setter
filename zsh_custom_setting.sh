5537993b0fee071c9945273d9717f405c77a5f65
Jihwan Jang <jjh0302@gmail.com>
2021-04-25 20:02:27 +0900

    change

--- zsh_custom_setting.sh
+++ zsh_custom_setting.sh
@@ -1,5 +1,4 @@
 #!bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 cp zshrc_custom_pwrlv99 ~/.zshrc
