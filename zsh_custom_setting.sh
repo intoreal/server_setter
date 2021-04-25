6ff36529a293a15dd16541eaa0ce37d8db648834
Jihwan Jang <42114277+Dalpeng@users.noreply.github.com>
2021-04-25 12:00:52 +0900

    Update zsh_custom_setting.sh

--- zsh_custom_setting.sh
+++ zsh_custom_setting.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-cp .zshrc_custom_pwrlv99 ~/.zshrc
+cp zshrc_custom_pwrlv99 ~/.zshrc
