d86c773b40cf04a7138bc0a016977389d4237e11
Jihwan Jang <42114277+Dalpeng@users.noreply.github.com>
2021-04-25 12:00:37 +0900

    Update zsh_basic_setting.sh

--- zsh_basic_setting.sh
+++ zsh_basic_setting.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 sudo apt-get install zsh -y
 sudo chsh -s $(which zsh) ubuntu
-cp .zshrc_basic_agnoster ~/.zshrc
+cp zshrc_basic_agnoster ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
