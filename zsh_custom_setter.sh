64f7526eb8400184b61a043f68eda0716688b30b
Jihwan Jang <jjh0302@gmail.com>
2021-04-25 21:36:05 +0900

    last

--- zsh_custom_setter.sh
+++ zsh_custom_setter.sh
@@ -6,3 +6,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp zshrc_custom ~/.zshrc
+zsh
