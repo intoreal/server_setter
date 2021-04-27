202955eec0bc04c5904affee685366e96cfd4e41
Jihwan Jang <jjh0302@gmail.com>
2021-04-27 21:57:41 +0900

    all

---
+++ zsh2_oh_my_zsh.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+echo "DO NOT CHOOSE change shell on EC2 environment."
+sleep 1
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- zsh3_custom_setter.sh
+++ zsh3_custom_setter.sh
@@ -1,9 +1,8 @@
 #!bin/bash
-echo "DO NOT CHOOSE change shell on EC2 environment."
-sleep 1
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/chrissicool/zsh-256color ~/.oh-my-zsh/custom/plugins/zsh-256color
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp zshrc_custom ~/.zshrc
+cp p10k_custom ~/.p10k.zsh
 zsh
